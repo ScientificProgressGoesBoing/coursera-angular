32f3300513918a92eb1e45aba0b4f6bfe49cef6c
unknown <bla@bla.org>
2016-09-30 23:52:36 +0200

    Add item.description to searched fields

--- mod3solution/javascript/app.js
+++ mod3solution/javascript/app.js
@@ -65,7 +65,7 @@ function MenuSearchService($http, ApiBasePath) {
             var item;
             var found = [];
             while (item = response.data.menu_items.shift()){
-              if (item.name.toLowerCase().indexOf(searchTerm) != -1){
+              if (item.name.toLowerCase().indexOf(searchTerm) != -1 || item.description.toLowerCase().indexOf(searchTerm) != -1){
                 found.push(item);
               }     
             }   
